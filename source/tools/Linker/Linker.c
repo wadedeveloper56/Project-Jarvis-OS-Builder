91be67054f5c774d8df5b45184ecbdd32ce7d95b
wadedeveloper56 <wade4615@gmail.com>
2025-06-12 16:13:13 -0500

    Added a linker and IDE projects

---
+++ source/tools/Linker/Linker.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <string.h>
+#include <ctype.h>
+#include <limits.h>
+#include <setjmp.h>
+
+
+int main()
+{
+    return 0;
+}
+
