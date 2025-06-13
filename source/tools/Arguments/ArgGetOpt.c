ba57c18f072dfe3bf8bafac0963bcdd6456a1c1d
wadedeveloper56 <wade4615@gmail.com>
2025-06-13 12:32:59 -0500

    arguments update

--- source/tools/Arguments/ArgGetOpt.c
+++ source/tools/Arguments/ArgGetOpt.c
@@ -440,27 +440,97 @@ start:
 	return (optchar);
 }
 
-int
-getopt(int nargc, char* const* nargv, const char* options)
-{
-
+int getopt(int nargc, char* const* nargv, const char* options) {
 	return (getopt_internal(nargc, nargv, options, NULL, NULL, 0));
 }
 
-int
-getopt_long(int nargc, char* const* nargv, const char* options,
-	const struct option* long_options, int* idx)
-{
-
-	return (getopt_internal(nargc, nargv, options, long_options, idx,
-		FLAG_PERMUTE));
+int getopt_long(int nargc, char* const* nargv, const char* options, const struct option* long_options, int* idx) {
+	return (getopt_internal(nargc, nargv, options, long_options, idx, FLAG_PERMUTE));
 }
 
-int
-getopt_long_only(int nargc, char* const* nargv, const char* options,
-	const struct option* long_options, int* idx)
-{
+int getopt_long_only(int nargc, char* const* nargv, const char* options, const struct option* long_options, int* idx) {
+	return (getopt_internal(nargc, nargv, options, long_options, idx, FLAG_PERMUTE | FLAG_LONGONLY));
+}
 
-	return (getopt_internal(nargc, nargv, options, long_options, idx,
-		FLAG_PERMUTE | FLAG_LONGONLY));
+void arg_cat(char** pdest, const char* src, size_t* pndest) {
+	char* dest = *pdest;
+	char* end = dest + *pndest;
+
+	while (dest < end - 1 && *dest != 0)
+		dest++;
+
+	while (dest < end - 1 && *src != 0)
+		*dest++ = *src++;
+
+	*dest = 0;
+
+	*pndest = (size_t)(end - dest);
+	*pdest = dest;
+}
+
+void arg_cat_optionv(char* dest, size_t ndest, const char* shortopts, const char* longopts, const char* datatype, int optvalue, const char* separator) {
+	separator = separator ? separator : "";
+
+	if (shortopts) {
+		const char* c = shortopts;
+		while (*c) {
+			char shortopt[3];
+
+			shortopt[0] = '-';
+			shortopt[1] = *c;
+			shortopt[2] = 0;
+
+			arg_cat(&dest, shortopt, &ndest);
+			if (*++c)
+				arg_cat(&dest, separator, &ndest);
+		}
+	}
+
+	if (shortopts && longopts)
+		arg_cat(&dest, separator, &ndest);
+
+	if (longopts) {
+		const char* c = longopts;
+		while (*c) {
+			size_t ncspn;
+
+			arg_cat(&dest, "--", &ndest);
+
+			ncspn = strcspn(c, ",");
+#if (defined(__STDC_LIB_EXT1__) && defined(__STDC_WANT_LIB_EXT1__)) || (defined(__STDC_SECURE_LIB__) && defined(__STDC_WANT_SECURE_LIB__))
+			strncat_s(dest, ndest, c, (ncspn < ndest) ? ncspn : ndest);
+#else
+			strncat(dest, c, (ncspn < ndest) ? ncspn : ndest);
+#endif
+			c += ncspn;
+
+			if (*c == ',') {
+				arg_cat(&dest, separator, &ndest);
+				c++;
+			}
+		}
+	}
+
+	if (datatype) {
+		if (longopts)
+			arg_cat(&dest, "=", &ndest);
+		else if (shortopts)
+			arg_cat(&dest, " ", &ndest);
+
+		if (optvalue) {
+			arg_cat(&dest, "[", &ndest);
+			arg_cat(&dest, datatype, &ndest);
+			arg_cat(&dest, "]", &ndest);
+		}
+		else
+			arg_cat(&dest, datatype, &ndest);
+	}
+}
+
+void arg_print_option_ds(ArgDstrPtr ds, const char* shortopts, const char* longopts, const char* datatype, const char* suffix) {
+	char syntax[200] = "";
+	suffix = suffix ? suffix : "";
+	arg_cat_optionv(syntax, sizeof(syntax) - 1, shortopts, longopts, datatype, 0, "|");
+	arg_dstr_cat(ds, syntax);
+	arg_dstr_cat(ds, (char*)suffix);
 }
