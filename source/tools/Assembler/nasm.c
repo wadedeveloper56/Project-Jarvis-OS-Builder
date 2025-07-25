bdec14438a615832958393dbbb85c3446918acd7
wadedeveloper56 <wade4615@gmail.com>
2025-07-25 11:26:46 -0500

    update to nasm port

--- source/tools/Assembler/nasm.c
+++ source/tools/Assembler/nasm.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "compiler.h"
+#include "nasm.h"
+#include "nasmlib.h"
+#include "nctype.h"
+#include "error.h"
+#include "saa.h"
+#include "raa.h"
+#include "floats.h"
+#include "stdscan.h"
+#include "insns.h"
+#include "preproc.h"
+#include "parser.h"
+#include "eval.h"
+#include "assemble.h"
+#include "labels.h"
+#include "outform.h"
+#include "listing.h"
+#include "iflag.h"
+#include "quote.h"
+#include "ver.h"
 
 int main(int argc, char **argv)
 {
