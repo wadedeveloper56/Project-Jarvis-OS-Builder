d564fe793f11360b860040cd2042c80ab11e1cd2
wadedeveloper56 <wade4615@gmail.com>
2025-08-04 11:56:34 -0500

    added dumpobj and watcom2msvc

---
+++ source/tools/Watcom2MSVC/Watcom2MSVC.c
@@ -0,0 +1,10 @@
+// Watcom2MSVC.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnWatcom2MSVC()
+{
+}
