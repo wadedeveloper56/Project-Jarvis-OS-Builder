ecb5b6e00fa6256a03a321c8bb8beb548eba22da
wadedeveloper56 <wade4615@gmail.com>
2025-08-08 10:12:28 -0500

    added a preprocessor project

---
+++ source/tools/Preproc/Preproc.cpp
@@ -0,0 +1,10 @@
+// Preproc.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnPreproc()
+{
+}
