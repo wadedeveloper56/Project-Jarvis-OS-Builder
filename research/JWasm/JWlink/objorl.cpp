19f3f47f33979b4aece2f4e54f8a0c520e05c2ab
wadedeveloper56 <wade4615@gmail.com>
2025-12-04 11:50:25 -0600

    update

--- research/JWasm/JWlink/objorl.cpp
+++ research/JWasm/JWlink/objorl.cpp
@@ -20,53 +20,51 @@ static char* FirstCodeSymName;
 static char* FirstDataSymName;
 static unsigned_32      ImpOrdinal;
 
-typedef struct readcache READCACHE;
-
 typedef struct readcache {
-    READCACHE* next;
-    void* data;
+	readcache* next;
+	void* data;
 } readcache;
 
 static readcache* ReadCacheList;
 
 void InitObjORL(void)
 {
-    ORLHandle = ORLInit(&ORLFuncs);
-    ReadCacheList = NULL;
+	ORLHandle = ORLInit(&ORLFuncs);
+	ReadCacheList = nullptr;
 }
 
 void ObjORLFini(void)
 {
-    ORLFini(ORLHandle);
+	ORLFini(ORLHandle);
 }
 
 static long ORLSeek(void* _list, long pos, int where)
 {
-    file_list* list = (file_list*)_list;
+	file_list* list = (file_list*)_list;
 
-    if (where == SEEK_SET) {
-        ORLFilePos = pos;
-    }
-    else if (where == SEEK_CUR) {
-        ORLFilePos += pos;
-    }
-    else {
-        ORLFilePos = list->file->len - pos;
-    }
-    return(ORLFilePos);
+	if (where == SEEK_SET) {
+		ORLFilePos = pos;
+	}
+	else if (where == SEEK_CUR) {
+		ORLFilePos += pos;
+	}
+	else {
+		ORLFilePos = list->file->len - pos;
+	}
+	return(ORLFilePos);
 }
 
 static void* ORLRead(void* _list, size_t len)
 {
-    file_list* list = (file_list*)_list;
-    void* result;
-    readcache* cache;
+	file_list* list = (file_list*)_list;
+	void* result;
+	readcache* cache;
 
-    result = CachePermRead(list, ORLFilePos, len);
-    ORLFilePos += len;
-    _ChkAlloc(readcache * ,cache, sizeof(readcache));
-    cache->next = ReadCacheList;
-    ReadCacheList = cache;
-    cache->data = result;
-    return(result);
+	result = CachePermRead(list, ORLFilePos, len);
+	ORLFilePos += len;
+	_ChkAlloc(readcache*, cache, sizeof(readcache));
+	cache->next = ReadCacheList;
+	ReadCacheList = cache;
+	cache->data = result;
+	return(result);
 }
---
+++ research/JWasm/orl/elfflhn.cpp
@@ -0,0 +1,7 @@
+#include "elfflhn.h"
+#include "orlhash.h"
+
+orl_return ElfRemoveFileLinks(elf_file_handle elf_file_hnd)
+{
+	return ORL_OKAY;
+}
