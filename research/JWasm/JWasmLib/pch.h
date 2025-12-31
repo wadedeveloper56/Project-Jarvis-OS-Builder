8646375a03540c91cb22841328e15b64ba69a8c2
wadedeveloper56 <wade4615@gmail.com>
2025-12-31 15:32:49 -0600

    update

---
+++ research/JWasm/JWasmLib/pch.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <ctype.h>
+#include <time.h>
+#include <float.h>
+#include <math.h>
+#include <stddef.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <setjmp.h>
+#if defined(__UNIX__) && ( defined(__GNUC__) || defined(__TINYC__) )
+#include <sys/mman.h>
+#endif
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#if defined( _M_IX86 ) && defined(__WATCOMC__)
+#include <i86.h>
+#endif
+
+#pragma warning(disable:4267)
+#pragma warning(disable:4244)
+#pragma warning(disable:4018)
+#pragma warning(disable:4311)
+#pragma warning(disable:4146)
+#pragma warning(disable:4113)
+#pragma warning(disable:5286)
+#pragma warning(disable:4312)
+#pragma warning(disable:4477)
+#pragma warning(disable:5287)
