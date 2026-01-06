7a24c8fe50f0e615c40df6d59dabc106dfad28aa
wadedeveloper56 <wade4615@gmail.com>
2026-01-06 11:15:46 -0600

    update

--- research/JWasm/MemoryTest/MemoryTest.cpp
+++ research/JWasm/MemoryTest/MemoryTest.cpp
@@ -14,6 +14,18 @@ namespace MemoryTest
 		{
 			void* buffer = AllocateMemory(1024);
 			Assert::IsNotNull(buffer, L"Memory allocation failure");
+			FreeMemory(buffer);
+		}
+		TEST_METHOD(ReallocateMemory_NotNull)
+		{
+			void* buffer = AllocateMemory(1024);
+			size_t size1 = _msize(buffer);
+			Assert::IsNotNull(buffer, L"Memory allocation failure");
+			buffer = ReallocateMemory(buffer, 2048);
+			size_t size2 = _msize(buffer);
+			Assert::IsNotNull(buffer, L"Memory reallocation failure");
+			Assert::AreNotEqual(size1, size2, L"Memory reallocation size failure");
+			FreeMemory(buffer);
 		}
 	};
 }
