27c7e67be341df2ee7e1e7ae4d4bd7d4aa624ca8
wadedeveloper56 <wade4615@gmail.com>
2026-02-20 09:34:50 -0600

    update

--- test/test1.c
+++ test/test1.c
@@ -1,12 +1,12 @@
-char var1[3];
-short var2[3];
-int var3[3];
-long var4[3];
-long long var5[3];
-float var6[3];
-double var=2.0;
-long double var8[3];
-int init = 9;
+char var1=5;
+short var2;
+int var3;
+long var4;
+long long var5;
+float var6;
+double var;
+long double var8;
+int init;
 
 void main()
 {
