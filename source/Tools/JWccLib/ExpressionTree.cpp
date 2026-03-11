19726ffdfb30d7a36867f07b5cb3b93d51da76af
wadedeveloper56 <wade4615@gmail.com>
2026-03-11 16:11:39 -0500

    update

--- source/Tools/JWccLib/ExpressionTree.cpp
+++ source/Tools/JWccLib/ExpressionTree.cpp
@@ -1,175 +1,97 @@
 #include "pch.h"
 #include "ExpressionTree.h"
 
-TreeNode::TreeNode(char data)
+using namespace WadeSpace;
+
+TreeNodeData::TreeNodeData()
+{
+    type = NT_NONE;
+    token1 = nullptr;
+    token2 = nullptr;
+    lexp = nullptr;
+    exp1 = nullptr;
+    exp2 = nullptr;
+    argumentList = nullptr;
+    identifier = nullptr;
+    initializerList = nullptr;
+    typeName = nullptr;
+    token3 = nullptr;
+    constant = nullptr;
+}
+
+TreeNodeData::TreeNodeData(
+    const NodeType type,
+    const TokenPtr token1,
+    const TokenPtr token2,
+    ExpressionTree* const lexp,
+    ExpressionTree* const exp1,
+    ExpressionTree* const exp2,
+    vector<ExpressionTree*>* argumentList,
+    TokenPtr identifier,
+    vector<Initializer*>* initializerList,
+    TypeName* typeName,
+    TokenPtr token3,
+    Constant* constant
+)
+{
+    this->type = type;
+    this->token1 = token1;
+    this->token2 = token2;
+    this->lexp = lexp;
+    this->exp1 = exp1;
+    this->exp2 = exp2;
+    this->argumentList = argumentList;
+    this->identifier = identifier;
+    this->initializerList = initializerList;
+    this->typeName = typeName;
+    this->token3 = token3;
+    this->constant = constant;
+}
+
+TreeNodeData::~TreeNodeData()
+{
+    delete token1;
+    delete token2;
+    delete lexp;
+    delete exp1;
+    delete exp2;
+    delete argumentList;
+    delete identifier;
+    delete initializerList;
+    delete typeName;
+    delete token3;
+    delete constant;
+}
+
+TreeNode::TreeNode()
+{
+    data = nullptr;
+    left = nullptr;
+    right = nullptr;
+}
+
+TreeNode::TreeNode(TreeNodeData* data, TreeNode* left, TreeNode* right)
 {
     this->data = data;
-    this->left = nullptr;
-    this->right = nullptr;
+    this->left = left;
+    this->right = right;
+}
+
+TreeNode::~TreeNode()
+{
+    delete data;
+    delete left;
+    delete right;
 }
 
 ExpressionTree::ExpressionTree()
 {
-    top = new stack<TreeNode*>();
+    stak = new stack<TreeNode*>();
+	tree = nullptr;
 }
 
 ExpressionTree::~ExpressionTree()
 {
-    clear();
-    delete top;
-}
-
-void ExpressionTree::clear()
-{
-    while (!top->empty())
-    {
-        TreeNode* node = top->top();
-        top->pop();
-        delete node;
-    }
-}
-
-void ExpressionTree::push(TreeNode* ptr)
-{
-    top->push(ptr);
-}
-
-TreeNode* ExpressionTree::pop()
-{
-    if (top->empty())
-        return nullptr;
-    TreeNode* node = top->top();
-    top->pop();
-    return node;
-}
-
-TreeNode * ExpressionTree::peek()
-{
-    if (top->empty())
-        return nullptr;
-    return top->top();
-}
-
-void ExpressionTree::insert(char val)
-{
-    if (isDigit(val))
-    {
-        TreeNode* nptr = new TreeNode(val);
-        push(nptr);
-    }
-    else if (isOperator(val))
-    {
-        TreeNode* nptr = new TreeNode(val);
-        nptr->left = pop();
-        nptr->right = pop();
-        push(nptr);
-    }
-    else
-    {
-        cout << "Invalid Expression" << endl;
-        return;
-    }
-}
-
-bool ExpressionTree::isDigit(char ch)
-{
-    return ch >= '0' && ch <= '9';
-}
-
-bool ExpressionTree::isOperator(char ch)
-{
-    return ch == '+' || ch == '-' || ch == '*' || ch == '/';
-}
-
-int ExpressionTree::toDigit(char ch)
-{
-    return ch - '0';
-}
-
-void ExpressionTree::buildTree(string eqn)
-{
-    for (int i = eqn.length() - 1; i >= 0; i--)
-        insert(eqn[i]);
-}
-
-double ExpressionTree::evaluate()
-{
-    return evaluate(peek());
-}
-
-double ExpressionTree::evaluate(TreeNode* ptr)
-{
-    if (ptr->left == NULL && ptr->right == NULL)
-        return toDigit(ptr->data);
-    else
-    {
-        double result = 0.0;
-        double left = evaluate(ptr->left);
-        double right = evaluate(ptr->right);
-        char op = ptr->data;
-        switch (op)
-        {
-            case '+':
-                result = left + right;
-                break;
-            case '-':
-                result = left - right;
-                break;
-            case '*':
-                result = left * right;
-                break;
-            case '/':
-                result = left / right;
-                break;
-            default:
-                result = left + right;
-                break;
-        }
-        return result;
-    }
-}
-
-void ExpressionTree::postfix()
-{
-    postOrder(peek());
-}
-
-void ExpressionTree::postOrder(TreeNode* ptr)
-{
-    if (ptr != NULL)
-    {
-        postOrder(ptr->left);
-        postOrder(ptr->right);
-        cout << ptr->data;
-    }
-}
-
-void ExpressionTree::infix()
-{
-    inOrder(peek());
-}
-
-void ExpressionTree::inOrder(TreeNode* ptr)
-{
-    if (ptr != NULL)
-    {
-        inOrder(ptr->left);
-        cout << ptr->data;
-        inOrder(ptr->right);
-    }
-}
-
-void ExpressionTree::prefix()
-{
-    preOrder(peek());
-}
-
-void ExpressionTree::preOrder(TreeNode* ptr)
-{
-    if (ptr != NULL)
-    {
-        cout << ptr->data;
-        preOrder(ptr->left);
-        preOrder(ptr->right);
-    }
+    delete stak;
+    delete tree;
 }
--- source/Tools/JWccLib/ExpressionTree.h
+++ source/Tools/JWccLib/ExpressionTree.h
@@ -1,38 +1,72 @@
 #pragma once
 
+#include "Token.h"
+#include "Constant.h"
+
 using namespace std;
 
-class TreeNode
+namespace WadeSpace
 {
-public:  
-    char data;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(char data);
-}; 
+	typedef enum _NodeType
+	{
+		NT_NONE, NT_ARRAY, NT_FUNCTION_CALL, NT_VAR_ACCESS, NT_INC, NT_DEC, NT_TYPECAST, NT_SIZEOF, NT_UNARY, NT_OP, NT_QUESTION
+	} NodeType;
 
-class ExpressionTree
-{
-    stack<TreeNode*>* top;
-public:
-	ExpressionTree();
-   	~ExpressionTree();
-    void clear();
-    void push(TreeNode* ptr);
-    TreeNode* pop();
-    TreeNode* peek();
-    void insert(char val);
-    bool isDigit(char ch);
-    bool isOperator(char ch);
-    int toDigit(char ch);
-    void buildTree(string eqn);
-    double evaluate();
-    double evaluate(TreeNode* ptr);
-    void postfix();
-    void postOrder(TreeNode* ptr);
-    void infix();
-    void inOrder(TreeNode* ptr);
-    void prefix();
-    void preOrder(TreeNode* ptr);
-};
+	class ExpressionTree;
+	class AssignmentExpression;
+	class Initializer;
+	class TypeName;
 
+	class TreeNodeData
+	{
+	public:
+		NodeType type;
+		TokenPtr token1;
+		TokenPtr token2;
+		ExpressionTree* lexp;
+		ExpressionTree* exp1;
+		ExpressionTree* exp2;
+		vector<ExpressionTree*>* argumentList;
+		TokenPtr identifier;
+		vector<Initializer*>* initializerList;
+		TypeName* typeName;
+		TokenPtr token3;
+		Constant* constant;
+		TreeNodeData();
+		TreeNodeData(
+			const NodeType type,
+			const TokenPtr token1,
+			const TokenPtr token2,
+			ExpressionTree* const lexp,
+			ExpressionTree* const exp1,
+			ExpressionTree* const exp2,
+			vector<ExpressionTree*>* argumentList,
+			TokenPtr identifier,
+			vector<Initializer*>* initializerList,
+			TypeName* typeName,
+			TokenPtr token3,
+			Constant* constant
+		);
+		~TreeNodeData();
+	};
+
+	class TreeNode
+	{
+	public:
+		TreeNodeData* data;
+		TreeNode* left;
+		TreeNode* right;
+		TreeNode();
+		TreeNode(TreeNodeData* data, TreeNode* left = nullptr, TreeNode* right = nullptr);
+		~TreeNode();
+	};
+
+	class ExpressionTree
+	{
+		stack<TreeNode*>* stak;
+		TreeNode* tree;
+	public:
+		ExpressionTree();
+		~ExpressionTree();
+	};
+}
