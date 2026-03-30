02078a975648461f941330039274124694be63db
wadedeveloper56 <wade4615@gmail.com>
2026-03-30 17:19:32 -0500

    update

--- source/Tools/JWccLib/AbstractDeclarator.h
+++ source/Tools/JWccLib/AbstractDeclarator.h
@@ -7,24 +7,21 @@ namespace WadeSpace
 {
 	class AbstractDeclarator
 	{ 
-	public:
-		AbstractDeclarator();
-		AbstractDeclarator(Pointer* pointer, DirectAbstractDeclarator* directAbstractDeclarator); 
-		virtual ~AbstractDeclarator();
-
-		AbstractDeclarator(const AbstractDeclarator& other); 
-		AbstractDeclarator(AbstractDeclarator&& other) noexcept;
-		AbstractDeclarator& operator=(const AbstractDeclarator& other);
-		AbstractDeclarator& operator=(AbstractDeclarator&& other) noexcept;
-
-		[[nodiscard]] Pointer* getPointer() const;
-		[[nodiscard]] DirectAbstractDeclarator* getDirectAbstractDeclarator() const;
-
-		[[nodiscard]] bool hasPointer() const;
-		[[nodiscard]] bool hasDirectAbstractDeclarator() const;
-
-	private:
 		Pointer* pointer;
 		DirectAbstractDeclarator* directAbstractDeclarator;
+	public:
+		AbstractDeclarator() = default;
+		AbstractDeclarator(Pointer* pointer, DirectAbstractDeclarator* directAbstractDeclarator);
+		virtual ~AbstractDeclarator() = default;
+		AbstractDeclarator(const AbstractDeclarator& other) = default;
+		AbstractDeclarator(AbstractDeclarator&& other) noexcept = default;
+		AbstractDeclarator& operator=(const AbstractDeclarator& other) = default;
+		AbstractDeclarator& operator=(AbstractDeclarator&& other) noexcept = default;
+		[[nodiscard]] Pointer* getPointer() const { return pointer; }
+		[[nodiscard]] DirectAbstractDeclarator* getDirectAbstractDeclarator() const { return directAbstractDeclarator; }
+		void setPointer(Pointer* pointer) { this->pointer = pointer; }
+		void setDirectAbstractDeclarator(DirectAbstractDeclarator* directAbstractDeclarator) { this->directAbstractDeclarator = directAbstractDeclarator; }
+		[[nodiscard]] bool hasPointer() const { return pointer != nullptr; }
+		[[nodiscard]] bool hasDirectAbstractDeclarator() const { return directAbstractDeclarator != nullptr; }
 	};
 }
