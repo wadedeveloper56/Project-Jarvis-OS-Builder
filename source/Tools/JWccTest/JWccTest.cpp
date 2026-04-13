13d6fec2b4de7f3c88610bdde6101d18a321be90
wadedeveloper56 <wade4615@gmail.com>
2026-04-13 13:01:46 -0500

    unit test update

--- source/Tools/JWccTest/JWccTest.cpp
+++ source/Tools/JWccTest/JWccTest.cpp
@@ -25,7 +25,7 @@ namespace JWccTest
 			Assert::AreEqual(0L, (long)compiler.use_count(), L"Use count should be 0 after reset");
 		}
 
-		TEST_METHOD(Compiler_Basic_Function)
+		TEST_METHOD(Compiler_Basic_Function1)
 		{
 			compiler = make_shared<Compiler>();
 			Assert::IsNotNull(compiler.get(), L"Pointer should not be null after allocation");
@@ -47,6 +47,59 @@ namespace JWccTest
 			Assert::IsTrue(data->getProgram() != nullptr, L"Program data should not be null after allocation");
 			Assert::IsTrue(data->getGenerator() != nullptr, L"Program data should not be null after allocation");
 			Assert::IsTrue(data->getProgram()->size() == 1, L"Program data should not be null after allocation");
+			auto func = data->getProgram()->at(0);
+			Assert::IsTrue(func != nullptr, L"ExternalDeclarationshould not be null after allocation");
+			Assert::AreEqual(1L, (long)compiler.use_count(), L"Use count should be 1 after allocation");
+			auto funcDef = func->getFunctionDefinition();
+			Assert::IsTrue(funcDef != nullptr, L"Function definition should not be null");
+			Assert::IsTrue(func->getDeclaration() == nullptr, L"declaration should not be mon-null");
+			compiler.reset();
+			Assert::IsNull(compiler.get(), L"Pointer should be null after reset");
+			Assert::AreEqual(0L, (long)compiler.use_count(), L"Use count should be 0 after reset");
+		}
+
+		TEST_METHOD(Compiler_Basic_Function2)
+		{
+			compiler = make_shared<Compiler>();
+			Assert::IsNotNull(compiler.get(), L"Pointer should not be null after allocation");
+			Assert::IsTrue(compiler->hasProgramData(), L"Program data should not be null after allocation");
+			Assert::IsTrue(compiler->hasTypedefList(), L"Typedef list should not be null after allocation");
+			Assert::IsTrue(compiler->hasFunctionList(), L"Function list should not be null after allocation");
+			Assert::IsTrue(compiler->hasStructList(), L"Struct list should not be null after allocation");
+			Assert::AreEqual(1L, (long)compiler.use_count(), L"Use count should be 1 after allocation");
+
+			istringstream input("int var1; int main() { return 0; }");
+			ostringstream output;
+			int exitCode = -1;
+			compiler->compileFile(input, output, exitCode);
+
+			Assert::IsNotNull(output.str().c_str(), L"There should be output");
+			Assert::IsTrue(compiler->hasProgramData(), L"Program data should not be null after allocation");
+			auto data = compiler->getProgramData();
+			Assert::IsTrue(data != nullptr, L"Program data should not be null after allocation");
+			Assert::IsTrue(data->getProgram() != nullptr, L"Program data should not be null after allocation");
+			Assert::IsTrue(data->getGenerator() != nullptr, L"Program data should not be null after allocation");
+			Assert::IsTrue(data->getProgram()->size() == 2, L"Program data should not be null after allocation");
+
+			auto func1 = data->getProgram()->at(0);
+			Assert::IsTrue(func1 != nullptr, L"ExternalDeclarationshould not be null after allocation");
+			Assert::AreEqual(1L, (long)compiler.use_count(), L"Use count should be 1 after allocation");
+			auto funcDef = func1->getFunctionDefinition();
+			auto variables = func1->getDeclaration();
+			Assert::IsTrue(funcDef == nullptr, L"Function definition should not be non-null");
+			Assert::IsTrue(variables != nullptr, L"declaration should not be null");
+
+			auto func2 = data->getProgram()->at(1);
+			Assert::IsTrue(func2 != nullptr, L"ExternalDeclarationshould not be null after allocation");
+			Assert::AreEqual(1L, (long)compiler.use_count(), L"Use count should be 1 after allocation");
+			auto funcDef2 = func2->getFunctionDefinition();
+			auto variables2 = func2->getDeclaration();
+			Assert::IsTrue(funcDef2 != nullptr, L"Function definition should not be null");
+			Assert::IsTrue(variables2 == nullptr, L"declaration should not be non-null");
+
+			compiler.reset();
+			Assert::IsNull(compiler.get(), L"Pointer should be null after reset");
+			Assert::AreEqual(0L, (long)compiler.use_count(), L"Use count should be 0 after reset");
 		}
 	};
 }
--- test/test1.asm
+++ test/test1.asm
@@ -1,4 +1,4 @@
-.x64p
+.386
 .model flat, c;
 option casemap : none
 
@@ -24,9 +24,9 @@ _var4 data1 <>
 _var5 data2 <>
 
 .code
-_main PROTO C _argc:SDWORD , _argv:QWORD ;
+_main PROTO C _argc:SDWORD , _argv:DWORD ;
 
-_main PROC C, _argc:SDWORD , _argv:QWORD 
+_main PROC C, _argc:SDWORD , _argv:DWORD 
 	mov eax,5
 	ret
 _main endp
