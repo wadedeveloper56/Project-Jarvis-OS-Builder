5d77a79045d386cfa9f7a636c1b78c0efd3229f1
wadedeveloper56 <wade4615@gmail.com>
2026-04-16 09:08:12 -0500

    Enable 32-bit build and register structs/unions
    Always register enum/struct/union types when a type_specifier is present (remove dependency on initDeclaratorsList) so type definitions are recorded even when followed by declarators. Update test runner to build 32-bit (-b 32) and adjust test sources/outputs for 32-bit (.386, DWORD calling convention). Remove the previous error output and add the generated test1.obj.

--- test/test1.asm
+++ test/test1.asm
@@ -1,19 +1,28 @@
-.x64p
+.386
 .model flat, c;
 option casemap : none
 
 
+type2 STRUCT
+	a SDWORD ?
+	b SDWORD ?
+type2 ENDS
+type3 UNION
+	a SDWORD ?
+	b SDWORD ?
+type3 ENDS
 
 .data
 .data?
 _var1 SDWORD  ?
 _var2 SDWORD  ?
 _var3 type2 <>
+_var4 type3 <>
 
 .code
-_main PROTO C _argc:SDWORD , _argv:QWORD ;
+_main PROTO C _argc:SDWORD , _argv:DWORD ;
 
-_main PROC C, _argc:SDWORD , _argv:QWORD 
+_main PROC C, _argc:SDWORD , _argv:DWORD 
 	mov eax,5
 	ret
 _main endp
