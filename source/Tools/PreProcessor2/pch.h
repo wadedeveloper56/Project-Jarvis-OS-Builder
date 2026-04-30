e66ccba858b4c67050cd4ef8d5ed36719954303b
wadedeveloper56 <wade4615@gmail.com>
2026-04-30 15:26:27 -0500

    Add PreProcessor2 tool and simplecpp sources
    Introduce a new PreProcessor2 static library: add MSVC project files (PreProcessor2.vcxproj, .filters, .user) and register it in source/tools/Tools.sln. Add headers (framework.h, pch.h), precompiled source (pch.cpp) and the full simplecpp implementation (simplecpp.cpp) to provide a C/C++ preprocessor library. Project settings configure C++20/ C11 standards, pch usage, output/int dirs for Debug/Release and Win32/x64, and various warning disables to match the imported library.

---
+++ source/Tools/PreProcessor2/pch.h
@@ -0,0 +1,56 @@
+#pragma once
+#if defined(_WIN32) || defined(__CYGWIN__) || defined(__MINGW32__)
+#  define _WIN32_WINNT 0x0602
+#  define NOMINMAX
+#  include <windows.h>
+#  undef ERROR
+#endif
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cstddef> // IWYU pragma: keep
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <istream>
+#include <limits>
+#include <list>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#ifdef SIMPLECPP_WINDOWS
+#  include <mutex>
+#endif
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#ifdef _WIN32
+#  include <direct.h>
+#else
+#  include <sys/stat.h>
+#endif
+
+
+#pragma warning(disable:4267)
+#pragma warning(disable:4244)
+#pragma warning(disable:4018)
+#pragma warning(disable:4311)
+#pragma warning(disable:4146)
+#pragma warning(disable:4113)
+#pragma warning(disable:5286)
+#pragma warning(disable:4312)
+#pragma warning(disable:4477)
+#pragma warning(disable:5287)
+#pragma warning(disable:4047)
+#pragma warning(disable:4065)
+#pragma warning(disable:4005)
+
