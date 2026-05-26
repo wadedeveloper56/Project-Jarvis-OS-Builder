d16a756c138985d117ac6087f490b7cd6a0a02aa
wadedeveloper56 <wade4615@gmail.com>
2026-05-26 08:26:54 -0500

    update

--- test/c064.asm
+++ test/c064.asm
@@ -1,13 +1,12 @@
 .x64
 .model flat, stdcall
 option casemap:none
-option frame:auto
 
 ; Win32 Prototypes
 GetCommandLineA PROTO STDCALL;
 ExitProcess PROTO STDCALL :DWORD;
 CommandLineToArgvA PROTO C, lpCmdLine:PTR BYTE, pNumArgs:PTR DWORD;
-LocalFree PROTO STDCALL :DWORD;
+LocalFree PROTO STDCALL :QWORD;
 
 ; C Runtime (CRT) Prototypes
 printf PROTO C :VARARG;
@@ -16,26 +15,42 @@ __getmainargs PROTO C, _Argc:PTR SDWORD, _Argv:PTR PTR SBYTE, _Env:PTR PTR SBYTE
 ;external main
 _main PROTO _argc:SDWORD , _argv:QWORD ;
 
-.data?
-    ; Define uninitialized variables for the CRT
-    pArgc   dd ?
-    pArgv   dq ?
-    pEnv    dq ?
-    pMode   dq ?
-
 .code
 
-mainCRTStartup PROC
-    ; Initialize C Runtime command line arguments
-    lea rax, pMode
-    lea rcx, pEnv
-    lea rdx, pArgv
-    lea rbx, pArgc
-    invoke __getmainargs, rbx, rdx, rcx, 0, rax
-	mov ecx,pArgc
-	mov rdx,pArgv
-    call _main
-	invoke ExitProcess, 0
-mainCRTStartup ENDP
+mainCRTStartup proc
+    LOCAL pCmdLine:QWORD
+    LOCAL pArgv:QWORD
+    LOCAL argc:DWORD
+    LOCAL i:DWORD
+
+    ; 1. Get the command-line string (Returns pointer to CHAR)
+    invoke GetCommandLineA
+    mov pCmdLine, rax
+    test rax, rax
+    jz error_exit
+
+    ; 2. Parse the command line into an array of pointers to CHAR
+    lea rcx, argc
+    mov rdx, pCmdLine
+    invoke CommandLineToArgvA, pArgv, addr argc
+    mov pArgv, rax
+    test rax, rax
+    jz error_exit
+
+    ; 3. call main
+    mov	rdx, pArgv
+	mov	ecx, argc
+	call _main
+	
+    ; 5. Free memory allocated by CommandLineToArgvA
+    invoke LocalFree, pArgv
+
+    ; Successful exit
+    invoke ExitProcess, 0
+
+error_exit:
+    invoke ExitProcess, 1
+
+mainCRTStartup endp
 
 END mainCRTStartup
