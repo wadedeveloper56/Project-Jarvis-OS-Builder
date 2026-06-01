cce1a2dcc185e9243575ac9f3a0a3d4c99bec753
wadedeveloper56 <wade4615@gmail.com>
2026-06-01 17:46:38 -0500

    Add Linker class and wire into JWlinker
    Move linking logic into a new Linker class: add Linker.h and Linker.cpp (stubbed link() returns 0) in JWlinkerLib. Update JWlinker.cpp to instantiate Linker, call link(), print and return the result. Update project files and filters to include the new files, add a project reference from JWlinker to JWlinkerLib, and add include paths so the header is found.

---
+++ source/Tools/JWlinkerLib/Linker.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "Linker.h"
+
+Linker::Linker()
+{
+}
+
+Linker::~Linker()
+{
+}
+
+int Linker::link()
+{
+    return 0;
+}
---
+++ source/Tools/JWlinkerLib/Linker.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Linker
+{
+public:
+    Linker();
+    ~Linker();
+	int link();
+};
+
