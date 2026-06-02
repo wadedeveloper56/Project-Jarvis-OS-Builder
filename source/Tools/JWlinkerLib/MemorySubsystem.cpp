b4a2133d8a978f341a244af39ad976cf235c35a9
wadedeveloper56 <wade4615@gmail.com>
2026-06-02 09:41:59 -0500

    Add MemorySubsystem wrappers and project refs
    Expose memory allocation API on MemorySubsystem by including Memory.h and adding AllocateMemory, FreeMemory and ReallocateMemory methods that forward to the global functions. Update JWlinkerLib.vcxproj to add the Memory include path and add ProjectReference entries for the File and Memory projects so JWlinkerLib builds/links against them.

--- source/Tools/JWlinkerLib/MemorySubsystem.cpp
+++ source/Tools/JWlinkerLib/MemorySubsystem.cpp
@@ -4,3 +4,9 @@
 MemorySubsystem::MemorySubsystem() {}
 
 MemorySubsystem::~MemorySubsystem() {}
+
+void* MemorySubsystem::AllocateMemory(size_t size) { return ::AllocateMemory(size); }
+
+void MemorySubsystem::FreeMemory(void* memoryBlock) { ::FreeMemory(memoryBlock); }
+
+void* MemorySubsystem::ReallocateMemory(void* memblock, size_t size) { return ::ReallocateMemory(memblock, size); }
--- source/Tools/JWlinkerLib/MemorySubsystem.h
+++ source/Tools/JWlinkerLib/MemorySubsystem.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include "Memory.h"
+
 class MemorySubsystem
 {
 public:
 	MemorySubsystem();
 	~MemorySubsystem();
+	void* AllocateMemory(size_t size);
+	void FreeMemory(void* memoryBlock);
+	void* ReallocateMemory(void* memblock, size_t size);
 };
 
