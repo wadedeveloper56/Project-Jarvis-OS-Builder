bd83dc3fa1ea5a0e43f50bc06c80031f9acb7c64
wadedeveloper56 <wade4615@gmail.com>
2026-06-02 16:12:16 -0500

    Add message catalogs and msgtexts array
    Add three message files (lnkerror.msg, rc.msg, wlink.msg) containing pick-style message definitions. Initialize a const char* const msgtexts[] in MessagingSubsystem.cpp by #undef/#define pick and including those .msg files, and add MSG_MAX_ERR_MSG_NUM in lnkerror.msg. Register the new .msg files in JWlinkerLib.vcxproj and its .filters so they are part of the project. These changes centralize linker/resource message strings and wire them into the messaging subsystem.

--- source/Tools/JWlinkerLib/MessagingSubsystem.cpp
+++ source/Tools/JWlinkerLib/MessagingSubsystem.cpp
@@ -1,6 +1,17 @@
 #include "pch.h"
 #include "MessagingSubsystem.h"
 
+const char* const msgtexts[] = {
+    "",
+    "",
+#undef pick
+#define pick( code, string )  string,
+#include "lnkerror.msg"
+#include "wlink.msg"
+#include "rc.msg"
+#undef pick
+};
+
 MessagingSubsystem::MessagingSubsystem()
 {
 }
