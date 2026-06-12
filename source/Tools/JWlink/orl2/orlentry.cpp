e56a7d617da9c6bc800b22632c025511d7bd432a
wadedeveloper56 <wade4615@gmail.com>
2026-06-12 09:53:06 -0500

    Remove ORL C source backends and update project
    Delete legacy ORL C implementation files under orl2/*/c (COFF, ELF, OMF) as part of a source reorganization. Add a top-level orl2/orlentry.cpp and rename/move orlflhnd.cpp and orlhash.cpp into the orl2 directory. Update orl2.vcxproj and its filters to reflect the new layout.

---
+++ source/Tools/JWlink/orl2/orlentry.cpp
@@ -0,0 +1,79 @@
+#include "pch.h"
+#include "orllevel.h"
+#include "orlentry.h"
+#include "orlflhnd.h"
+#include "pcobj.h"
+
+elf_handle ElfInit(orl_funcs* funcs)
+{
+	return NULL;
+}
+
+coff_handle CoffInit(orl_funcs* funcs)
+{
+	return NULL;
+}
+
+omf_handle OmfInit(orl_funcs* funcs)
+{
+	return NULL;
+}
+
+orl_handle ORLInit(orl_funcs* funcs)
+{
+	orl_handle orl_hnd = (orl_handle)funcs->alloc(sizeof(orl_handle_struct));
+	if (!orl_hnd) return(NULL);
+	orl_hnd->error = ORL_OKAY;
+	orl_hnd->funcs = funcs;
+	orl_hnd->elf_hnd = ElfInit(funcs);
+	if (!(orl_hnd->elf_hnd))
+	{
+		funcs->free(orl_hnd);
+		return(NULL);
+	}
+	orl_hnd->coff_hnd = CoffInit(funcs);
+	if (!(orl_hnd->coff_hnd))
+	{
+		funcs->free(orl_hnd);
+		return(NULL);
+	}
+	orl_hnd->omf_hnd = OmfInit(funcs);
+	if (!(orl_hnd->omf_hnd))
+	{
+		funcs->free(orl_hnd);
+		return(NULL);
+	}
+	orl_hnd->first_file_hnd = NULL;
+	return(orl_hnd);
+}
+
+orl_return CoffFini(coff_handle coff_hnd)
+{
+	return(ORL_OKAY);
+}
+
+orl_return ElfFini(elf_handle elf_hnd)
+{
+	return(ORL_OKAY);
+}
+
+orl_return OmfFini(omf_handle oh)
+{
+	return(ORL_OKAY);
+}
+
+orl_return ORLFini(orl_handle orl_hnd) 
+{
+	orl_return                  error;
+
+	if ((error = ElfFini(orl_hnd->elf_hnd)) != ORL_OKAY) return(error);
+	if ((error = CoffFini(orl_hnd->coff_hnd)) != ORL_OKAY) return(error);
+	if ((error = OmfFini(orl_hnd->omf_hnd)) != ORL_OKAY) return(error);
+	while (orl_hnd->first_file_hnd)
+	{
+		error = ORLRemoveFileLinks(orl_hnd->first_file_hnd);
+		if (error != ORL_OKAY) return(error);
+	}
+	orl_hnd->funcs->free(orl_hnd);
+	return(ORL_OKAY);
+}
