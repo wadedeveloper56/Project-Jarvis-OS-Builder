6b2242b7df5083d937eae0ac6354c4b2365a38ae
wadedeveloper56 <wade4615@gmail.com>
2026-07-15 14:28:41 -0500

    update

--- source/Tools/JWlink/JWlinkerLib/Carve.cpp
+++ source/Tools/JWlink/JWlinkerLib/Carve.cpp
@@ -1,10 +1,57 @@
 #include "pch.h"
 #include "Carve.h"
+#include "MemorySubsystem.h"
+#include "Structs.h"
 
-Carve::Carve()
+Carve::Carve(MemorySubsystem* memorySubsystem) : memorySubsystem(memorySubsystem), cv(nullptr)
 {
 }
 
 Carve::~Carve()
 {
+	if (cv != nullptr)
+	{
+		CarveDestroy();
+	}
+}
+
+void Carve::CarveCreate(size_t elm_size, size_t blk_size)
+{
+	elm_size = (elm_size + (sizeof(int) - 1)) & ~(sizeof(int) - 1);
+	if (elm_size < sizeof(free_t))
+	{
+		elm_size = sizeof(free_t);
+	}
+	_ChkAlloc(cv_t*, cv, sizeof(*cv));
+	cv->elm_size = elm_size;
+	cv->blk_size = blk_size;
+	cv->elm_count = cv->blk_size / cv->elm_size;
+	cv->blk_top = cv->elm_count * elm_size;
+	cv->blk_count = 0;
+	cv->blk_list = NULL;
+	cv->free_list = NULL;
+	cv->blk_map = NULL;
+	cv->size_chg = FALSE;
+}
+
+void Carve::CarveDestroy()
+{
+	blk_t* cur;
+	blk_t* next;
+
+	if (cv != NULL)
+	{
+		if (cv->blk_map != NULL)
+		{
+			_LnkFree(cv->blk_map);
+		}
+		cur = cv->blk_list;
+		while (cur != NULL)
+		{
+			next = cur->next;
+			_LnkFree(cur);
+			cur = next;
+		}
+		_LnkFree(cv);
+	}
 }
--- source/Tools/JWlink/JWlinkerLib/Carve.h
+++ source/Tools/JWlink/JWlinkerLib/Carve.h
@@ -1,10 +1,48 @@
 #pragma once
 
+#include "MemorySubsystem.h"
+#include "Structs.h"
+
+typedef struct blk blk_t;
+typedef struct free_t free_t;
+
+struct blk
+{
+    blk_t* next;
+    unsigned    index;
+    unsigned    modified : 1;
+    unsigned : 15;
+    char        data[1];
+};
+
+struct free_t
+{
+    free_t* next_free;
+};
+
+#define _REMOVE_FROM_FREE( pcv, p ) \
+    { \
+        free_t *head = pcv->free_list; \
+        pcv->free_list = head->next_free; \
+        p = head; \
+    }
+
+#define _ADD_TO_FREE( fl, p ) \
+    { \
+        free_t *node = (free_t *) (p); \
+        node->next_free = (fl); \
+        (fl) = node; \
+    }
+
+
 class Carve
 {
+    MemorySubsystem* memorySubsystem;
+    cv_t* cv;
 public:
-
-	Carve();
+	Carve(MemorySubsystem* memorySubsystem);
 	~Carve();
+    void CarveCreate(size_t elm_size, size_t blk_size);
+    void CarveDestroy();
 };
 
