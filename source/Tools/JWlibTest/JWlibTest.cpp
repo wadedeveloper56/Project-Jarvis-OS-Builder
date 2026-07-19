b6dd9b51dfe0bc4cae8310f1bbba891bc9b3cdf9
wadedeveloper56 <wade4615@gmail.com>
2026-07-19 08:59:01 -0500

    Add native unit test projects and update Tools.sln
    Add scaffolded Visual Studio native unit test projects for JWasmTest, JWlibTest and JWlinkTest (vcxproj, .filters, .user, pch.h/pch.cpp and a placeholder test file each). Update source/tools/Tools.sln to include the new projects and solution configuration entries. Binary vc140.pdb files in several tool subprojects were also updated.

---
+++ source/Tools/JWlibTest/JWlibTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace JWlibTest
+{
+	TEST_CLASS(JWlibTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ source/Tools/JWlinkTest/JWlinkTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace JWlinkTest
+{
+	TEST_CLASS(JWlinkTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
