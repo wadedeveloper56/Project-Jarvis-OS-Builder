8f911adeb681bbb71374befe0b421a6d91f38c0b
wadedeveloper56 <wade4615@gmail.com>
2026-08-17 08:38:50 -0500

    update

---
+++ source/Tools/JWasm/resource.h
@@ -0,0 +1,28 @@
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#ifndef WINVER
+#define WINVER 0x0500
+#endif
+
+#ifndef _WIN32_IE
+#define _WIN32_IE 0x0501
+#endif
+
+#ifndef _WIN32_WINDOWS
+#define _WIN32_WINDOWS 0x0410
+#endif
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0500
+#endif
+#define PACKAGE "dosbox-x"
+#define PACKAGE_BUGREPORT "https://github.com/joncampbell123/dosbox-x/issues"
+#define PACKAGE_NAME "dosbox-x"
+#define PACKAGE_STRING "dosbox-x 2026.08.02"
+#define PACKAGE_TARNAME "dosbox-x"
+#define PACKAGE_URL "https://dosbox-x.com"
+#define PACKAGE_VERSION "2026.08.02"
+#define VERSION "2026.08.02"
+#define COPYRIGHT_YEAR "2025"
