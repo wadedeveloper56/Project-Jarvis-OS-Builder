bf95260138febd83600bab4cd51609e449e395b3
wadedeveloper56 <wade4615@gmail.com>
2026-09-02 09:09:40 -0500

    update

--- source/Tools/JWlink/orl/coff/c/coffentr.c
+++ source/Tools/JWlink/orl/coff/c/coffentr.c
@@ -29,6 +29,7 @@
 *
 ****************************************************************************/
 
+#include "pch.h"
 #include "coffentr.h"
 #include "coffflhn.h"
 #include "cofflwlv.h"
--- source/Tools/JWlink/orl/omf/c/omfmunge.c
+++ source/Tools/JWlink/orl/omf/c/omfmunge.c
@@ -30,6 +30,7 @@
 ****************************************************************************/
 
 
+#include "pch.h"
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
